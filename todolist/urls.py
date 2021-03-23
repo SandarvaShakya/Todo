a719bc4e0a79e8bc353c73f076d31d098c65000a
SandarvaShakya <sandarvashakya34@gmail.com>
2021-03-23 15:59:41 +0545

    first commit

---
+++ todolist/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import delete, index, completed
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('completed/<int:primary_key>/', completed, name='completed'),
+    path('delete/<int:primary_key>/', delete, name='delete'),
+]
---
+++ todolist/views.py
@@ -0,0 +1,37 @@
+from todolist.models import TaskList
+from django.shortcuts import redirect, render
+from .forms import TaskForm
+
+
+def index(request):
+    task = TaskList.objects.all()
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    context = {'form': form, 'tasks': task}
+    return render(request, 'todolist/index.html', context)
+
+
+def completed(request, primary_key):
+    task = TaskList.objects.get(id=primary_key)
+    task.completed = True
+    task.save()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+
+def delete(request, primary_key):
+    task = TaskList.objects.get(id=primary_key)
+    task.delete()
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
